f1de25bf611296c74c93481a6300c57ee33e233f
ashikapl <ashikapanchal54@gmail.com>
2024-02-12 19:50:38 +0530

    testing

---
+++ debug/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, from_rod, to_rod);
+}
+
+int main()
+{
+}
