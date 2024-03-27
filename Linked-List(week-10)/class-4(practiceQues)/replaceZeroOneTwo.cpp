da1e8389706fec3cfc9d8cef4f4b9d71df3124ad
ashikapl <ashikapanchal54@gmail.com>
2024-03-27 17:22:27 +0530

    linked-list

--- Linked-List(week-10)/class-4(practiceQues)/replaceZeroOneTwo.cpp
+++ Linked-List(week-10)/class-4(practiceQues)/replaceZeroOneTwo.cpp
@@ -76,14 +76,114 @@ void sortZeroesOnesAndTwos(Node *&head)
     }
 }
 
+Node *sort2(Node *&head)
+{
+    if (head == NULL)
+    {
+        cout << "LL is empty" << endl;
+        return NULL;
+    }
+    if (head->next == NULL)
+    {
+        return head;
+    }
+
+    Node *zeroHead = new Node(-1);
+    Node *zeroTail = zeroHead;
+
+    Node *oneHead = new Node(-1);
+    Node *oneTail = oneHead;
+
+    Node *twoHead = new Node(-1);
+    Node *twoTail = twoHead;
+
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        if (curr->data == 0)
+        {
+            // take out zero wali node
+            Node *temp = curr;
+            curr = curr->next;
+            temp->next = NULL;
+
+            // append zero node in zeroTail LL
+            zeroTail->next = temp;
+            zeroTail = temp;
+        }
+        else if (curr->data == 1)
+        {
+            // take out zero wali node
+            Node *temp = curr;
+            curr = curr->next;
+            temp->next = NULL;
+
+            // append zero node in zeroTail LL
+            oneTail->next = temp;
+            oneTail = temp;
+        }
+        else if (curr->data == 2)
+        {
+            // take out zero wali node
+            Node *temp = curr;
+            curr = curr->next;
+            temp->next = NULL;
+
+            // append zero node in zeroTail LL
+            twoTail->next = temp;
+            twoTail = temp;
+        }
+    }
+
+    // all zero , one and two nodes are prepared
+    // Now join them and remove extra node of (-1)
+
+    // modify oneHead node
+    Node *temp = oneHead;
+    oneHead = oneHead->next;
+    temp->next = NULL;
+    delete temp;
+
+    // modify twoHead node
+    temp = twoHead;
+    twoHead = twoHead->next;
+    temp->next = NULL;
+    delete temp;
+
+    // Join list
+    if (oneHead != NULL)
+    {
+        zeroTail->next = oneHead;
+        if (twoHead != NULL)
+        {
+            oneTail->next = twoHead;
+        }
+    }
+    else
+    {
+        if (twoHead != NULL)
+        {
+            zeroTail->next = twoHead;
+        }
+    }
+
+    temp = zeroHead;
+    zeroHead = zeroHead->next;
+    temp->next = NULL;
+    delete temp;
+
+    // return head of modified node
+    return zeroHead;
+}
+
 int main()
 {
     Node *head = new Node(1);
-    Node *second = new Node(2);
-    Node *third = new Node(2);
+    Node *second = new Node(0);
+    Node *third = new Node(0);
     Node *fourth = new Node(0);
-    Node *fifth = new Node(0);
-    Node *sixth = new Node(1);
+    Node *fifth = new Node(2);
+    Node *sixth = new Node(2);
 
     head->next = second;
     second->next = third;
@@ -95,8 +195,11 @@ int main()
     cout << "Input LL: ";
     print(head);
 
-    sortZeroesOnesAndTwos(head);
+    // sortZeroesOnesAndTwos(head);
+    // print(head);
 
+    // Node *temp = NULL;
+    head = sort2(head);
     cout << "Output LL: ";
     print(head);
 
